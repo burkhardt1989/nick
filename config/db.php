c5523016b4c6ae9eb9cee36e038013fcca2223c8
wuchang <695936262@qq.com>
2017-07-13 17:17:08 +0800

    init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=attend',
+    'username' => 'root',
+    'password' => 'yysnowy',
+    'charset' => 'utf8',
+];
---
+++ config/params.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'adminEmail' => 'changwu1989@163.com',
+];
